9ca66c0f1a1dcbf2a7ad30ee064558d3a5437488
Brandon Reid <brandonreid2@my.unt.edu>
2017-10-19 08:58:25 -0500

    finished program 2

--- program2_bar0191/Server.java
+++ program2_bar0191/Server.java
@@ -91,41 +91,58 @@ class ServerThread extends Thread {
         this.sock = sock;
     }
     
+    // get and manage HTTP Request for client
     public String getHTTPRequest(String url) throws IOException {
+
+        // initialize variables
         int PORT               = 80;
         String errorStatus     = "ERROR: 400 Bad Request";
-        StringBuilder response = new StringBuilder();
-        boolean isContent      = false;
-        String line            = null;
+        StringBuilder response = new StringBuilder();       
+        boolean isContent      = false;                   
+        String line            = null;                      
         String statusCode      = null;
         PrintWriter output     = null;
         BufferedReader input   = null;
-        String request         = "GET / HTTP/1.1\r\nConnection: close\r\nHost:"+url+"\r\n\r\n";
         FileWriter fileOutput  = null;
         boolean needsCached    = false;
+        url                    = url.trim();
 
+        // build HTTP GET request based on url
+        String request = "GET / HTTP/1.1\r\nConnection: close\r\nHost:"+url+"\r\n\r\n";
+
+        // try to connect to desired url using socket implementation
         try (Socket sock = new Socket(url, PORT)) {
+            // initialize streams
             output = new PrintWriter(sock.getOutputStream());
             input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 
+            // write request to output stream
             output.write(request);
-            output.flush();
+            output.flush(); // flush for any extra chars
 
+            // read first initial line of response
             line = input.readLine();
-            statusCode = line; 
+            statusCode = line;  // set status code
 
+            // if status code is 200 OK
             if (line.startsWith("HTTP/1.1 200 OK")) {
-
+                
+                // initialize file scanner and arraylist of sites
                 Scanner fileScan = new Scanner(new File("list.txt"));
                 ArrayList<String> list = new ArrayList<String>();
+
+                // add each site in list.txt to an arraylist
                 while (fileScan.hasNextLine()) {
                     list.add(fileScan.nextLine());
                 }
+
+                // if url entered is not in the array list
                 if (!list.contains(url)) {
-                    list.add(0, url);
-                    needsCached = true;
+                    list.add(0, url);   // add url to array list
+                    needsCached = true; // setup for caching
                 }
-                
+
+                // if the array list has more then 5 urls, delete oldest one
                 if (list.size() > 5) {
                     File deleteFile = new File(list.get(list.size() - 1));
                     if(deleteFile.delete()) {
@@ -135,13 +152,20 @@ class ServerThread extends Thread {
                     }
                     list.remove(list.size() - 1);
                 }
+
+                // open new filewriter output to write to list.txt
                 fileOutput = new FileWriter("list.txt");
+
+                // write new arraylist of cached sites to list.txt
                 for (String str: list) {
                     fileOutput.write(str + "\n");
                 }
                 fileOutput.close();
-                System.out.println(list);
+                
+                // loop through lines in response 
                 while((line = input.readLine()) != null) {
+
+                    // check if content (HTML) is present in respone
                     if (!isContent) {
                         if (line.toLowerCase().startsWith("<!doctype html") || (line.toLowerCase().startsWith("<html"))) {
                             isContent = true;
@@ -149,23 +173,28 @@ class ServerThread extends Thread {
                             continue;
                         }
                     }
+                    // build client response
                     response.append(line + " ");    
                 }
+
+                // if response needs cached, write html to new file
                 if (needsCached) {
                     fileOutput = new FileWriter(url);
                     fileOutput.write(response.toString());
                     fileOutput.close();
                     needsCached = false;
                 }
+                
             } else {
+                // if response code is not 200 OK
                 response.append(statusCode + " ");
                 while((line = input.readLine()) != null) {
                     response.append(line + " ");
                 }
             }
-            return response.toString(); 
+            return response.toString(); // return response to client
         } catch(IOException e) {
-            return errorStatus;
+            return errorStatus; // will only return if 400 error
         }
     }
 
